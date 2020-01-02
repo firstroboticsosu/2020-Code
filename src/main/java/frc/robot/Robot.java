40d5b8bb24a677d474043c7141c3d665dd201cfe
coalman321 <coalman321@users.noreply.github.com>
2020-01-02 10:31:04 -0500

    add base code and switch to timed robot (oops)

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2020 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -7,90 +7,131 @@
 
 package frc.robot;
 
-import edu.wpi.first.hal.HAL;
-import edu.wpi.first.wpilibj.RobotBase;
-import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import java.util.Arrays;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+
+import frc.lib.loops.Looper;
+import frc.lib.statemachine.Scheduler;
+import frc.lib.statemachine.StateMachine;
 import frc.lib.util.VersionData;
+import frc.robot.statemachines.TestMach;
+import frc.robot.subsystems.*;
 
 /**
  * The VM is configured to automatically run this class. If you change the name
  * of this class or the package after creating this project, you must also
  * update the build.gradle file in the project.
  */
-public class Robot extends RobotBase {
+public class Robot extends TimedRobot {
+
+    SubsystemManager manager;
+    Looper enabledLooper;
+    Looper disabledLooper;
+
     public void robotInit() {
+        //Create instance of subsystem manager
+        this.manager = new SubsystemManager(Arrays.asList(
+            PoseEstimator.getInstance(),
+            Drive.getInstance()
+            ), true);
+
+        //Create instance of loopers for enabled and disabled modes
+        this.enabledLooper = new Looper();
+        this.disabledLooper = new Looper();
+
+        //Register looper instances to the manager
+        this.manager.registerEnabledLoops(this.enabledLooper);
+        this.manager.registerDisabledLoops(this.disabledLooper);
+
+        //Final cleanup after initialization
         VersionData.WriteBuildInfoToDashboard();
     }
 
-    public void disabled() {
+    @Override
+    public void robotPeriodic() {
+        Drive.getInstance().outputTelemetry();
+    }
+
+    @Override
+    public void disabledInit() {
+        //Stop current looper and start new looper
+        enabledLooper.stop();
+        Scheduler.assertStop();
+        StateMachine.assertStop();
+
+        //reset any subsystems
+
+        //start new looper
+        disabledLooper.start();
+    }
+
+    @Override
+    public void disabledPeriodic() {
         
     }
 
-    public void autonomous() {
-
-    }
-
-    public void teleop() {
-
-    }
-
-    public void test() {
-
-    }
-
-    //Ignore anything below this comment. Modifying it for no reason 
-    //  could cause issues with DS interaction
-
-    private volatile boolean m_exit;
-
-    @SuppressWarnings("PMD.CyclomaticComplexity")
     @Override
-    public void startCompetition() {
-        robotInit();
+    public void autonomousInit() {
+        //Stop current looper and start new looper
+        disabledLooper.stop();
+        
+        //reset any subsystems
+        Drive.getInstance().reset();
+        PoseEstimator.getInstance().reset();
 
-        // Tell the DS that the robot is ready to be enabled
-        HAL.observeUserProgramStarting();
+        //start new looper
+        enabledLooper.start();
 
-        while (!Thread.currentThread().isInterrupted() && !m_exit) {
-            if (isDisabled()) {
-            m_ds.InDisabled(true);
-            disabled();
-            m_ds.InDisabled(false);
-            while (isDisabled()) {
-                m_ds.waitForData();
-            }
-            } else if (isAutonomous()) {
-            m_ds.InAutonomous(true);
-            autonomous();
-            m_ds.InAutonomous(false);
-            while (isAutonomous() && !isDisabled()) {
-                m_ds.waitForData();
-            }
-            } else if (isTest()) {
-            LiveWindow.setEnabled(true);
-            Shuffleboard.enableActuatorWidgets();
-            m_ds.InTest(true);
-            test();
-            m_ds.InTest(false);
-            while (isTest() && isEnabled()) {
-                m_ds.waitForData();
-            }
-            LiveWindow.setEnabled(false);
-            Shuffleboard.disableActuatorWidgets();
-            } else {
-            m_ds.InOperatorControl(true);
-            teleop();
-            m_ds.InOperatorControl(false);
-            while (isOperatorControl() && !isDisabled()) {
-                m_ds.waitForData();
-            }
-            }
-        }
+        //start the state machine
+        StateMachine.runMachine(new TestMach());
     }
 
     @Override
-    public void endCompetition() {
-        m_exit = true;
+    public void autonomousPeriodic() {
+
     }
+
+    @Override
+    public void teleopInit() {
+        //Stop current looper and start new looper
+        disabledLooper.stop();
+        
+        //reset any subsystems
+        Drive.getInstance().reset();
+        PoseEstimator.getInstance().reset();
+
+        //start new looper
+        enabledLooper.start();
+
+        //Start scheduler
+        Scheduler.runScheduler();
+    }
+
+    @Override
+    public void teleopPeriodic() {
+
+    }
+
+    @Override
+    public void testInit() {
+        //Stop current looper
+        disabledLooper.stop();
+        
+        //reset any subsystems
+        Drive.getInstance().reset();
+        PoseEstimator.getInstance().reset();
+
+        //start new looper
+        enabledLooper.start();
+
+        //Start scheduler
+        Scheduler.runScheduler();
+    }
+
+    @Override
+    public void testPeriodic() {
+
+    }
+
 }
